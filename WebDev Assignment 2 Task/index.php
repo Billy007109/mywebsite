617ff55cf7b5f6ce29f812e825e74f6bf04683e0
Billy007109 <161894954+Billy007109@users.noreply.github.com>
2024-12-08 20:41:51 +0000

    Add files via upload

---
+++ WebDev Assignment 2 Task/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+
+    
+
+    <body>
+        <h1>You are loged in</h1>
+            
+
+    
+    </body>
+</html>
